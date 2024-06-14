149ad971cc34712463b4d331ba3b8eae1b32cf73
Imran Razak <77019152+ImranRazak1@users.noreply.github.com>
2024-06-14 16:49:00 +0100

    Initial Commit

---
+++ Sundance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sundance
+//
+//  Created by Imran razak on 14/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sundance/SundanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SundanceApp.swift
+//  Sundance
+//
+//  Created by Imran razak on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SundanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
